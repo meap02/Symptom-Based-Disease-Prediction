0be6fed140b80bcc9477aac15c8cb582674a0330
meap02 <79549780+meap02@users.noreply.github.com>
2023-05-07 22:50:51 -0700

    Fixed issue with data loading and integrated OHE

--- models/randomforest/randomforest.py
+++ models/randomforest/randomforest.py
@@ -4,28 +4,28 @@ import matplotlib.pyplot as plt
 import pickle
 import json
 # -------------------------- Models -------------------------- #
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
 # -------------------------- Preprocessing -------------------------- #
-from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
+from sklearn.preprocessing import LabelEncoder, OrdinalEncoder, TargetEncoder, OneHotEncoder
 # -------------------------- Metrics -------------------------- #
 from sklearn.model_selection import train_test_split, cross_val_score
 
 
-
 # -------------------------- Data -------------------------- #
-dataframe = pd.read_csv('data/patil/dataset.csv').dropna(axis=1).sample(frac=1)
-x = dataframe.drop("Disease", axis=1)
-y = dataframe["Disease"]
+df = pd.read_csv('data/patil/dataset.csv').sample(frac=1)
+x = df.drop("Disease", axis=1)
+y = df["Disease"]
 #print(dataframe.drop('Disease', axis=1))
 le = LabelEncoder()
-oe = OrdinalEncoder()
+#te = TargetEncoder(target_type='binary', cv=4)
+one_e = OneHotEncoder()
+
 y = le.fit_transform(y)
-x = oe.fit_transform(x)
-
-
-
-#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.16, train_size=0.84)
-
+one_e.fit(x)
+x = one_e.fit_transform(x)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.4, train_size=.6)
+#x_train = te.fit_transform(x_train, y_train)
+#x_test = te.transform(x_test)
 # -------------------------- Models -------------------------- #
 best_accuracy = 0
 best_estimators = 0
@@ -34,24 +34,29 @@ best_criteria = ""
 best_bootstap = False
 best_min_samples_split = 0
 best_model = None
-
-with open('models/randomforest/randomforest.json', 'r') as f:
-    data = json.load(f)
-    best_accuravy = data['accuracy']
-    best_estimators = data['estimators']
-    best_depth = data['depth']
-    best_criteria = data['criteria']
-    best_bootstap = data['bootstrap']
-with open('models/randomforest/randomforest.pkl', 'rb') as f:
-    best_model = pickle.load(f)   
-
-for n in range (1, 50):
-    for max in range(1, 20):
-        for criterion in ['gini', 'entropy', 'log_loss']:
+'''
+try:
+    with open('models/randomforest/randomforest.json', 'r') as f:
+        data = json.load(f)
+        best_accuravy = data['accuracy']
+        best_estimators = data['estimators']
+        best_depth = data['depth']
+        best_criteria = data['criteria']
+        best_bootstap = data['bootstrap']
+    with open('models/randomforest/randomforest.pkl', 'rb') as f:
+        best_model = pickle.load(f)
+except Exception as e:
+    print(e)
+'''
+for n in range (1, 20):
+    for max in range(1, 17):
+        for criterion in ['entropy', 'log_loss']:
             for bootstap in [True, False]:
                 print("Attempting with {n} estimators, {max} depth, {criterion} criterion, and {bootstap} bootstap".format(n=n, max=max, criterion=criterion, bootstap=bootstap))
                 clf = RandomForestClassifier(n_estimators=n, max_depth=max, criterion='entropy', bootstrap=bootstap)
-                score = np.mean(cross_val_score(clf, x, y, cv=6, scoring='recall_macro'))
+                score = cross_val_score(clf, x, y, cv=6).mean()
+                #clf.fit(x_train, y_train)
+                #score = clf.score(x_test, y_test)
                 if score > best_accuracy: 
                     best_accuracy = score
                     best_estimators = n
